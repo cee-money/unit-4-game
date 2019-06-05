34df655f9efe5e6eadc1a609734a1a4bf0a9c1bb
cmoney <moneymaker.carolyn@gmail.com>
2019-06-04 19:28:54 -0600

    pseudo coded js file with necessary steps, created HTML display with Bootstrap and overrode some styles in css file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+// Create variables to track
+    // Wins counter
+    // Losses counter
+    // Goal for the round (random number)
+    // Score
+    // Values of each crystal
+wins = 0;
+losses = 0;
+var randomNum = ;
+var score;
+var firstCrystalVal;
+var secondCrystalVal;
+var thirdCrystalVal;
+var fourthCrystalVal;
+
+
+// Create a function to start/reset the game
+// 	Update win and loss text
+// 	Pick a new goal (new random number) between 19-120
+// 	Update this on the page
+// 	Create 4 new values for the 4 crystals between 1 and 12
+// 	Assign 4 new values for the 4 crystals
+// 	(Bonus: Shuffle the crystals on the screen or show a message that values have changed)
+// 	Reset score back down to 0
+// 	Update the score text on the page
+
+// Create a function to test win/lose
+// 	If the player’s score === the goal
+// 		Wins++
+// 	Call reset function
+// 	If the player’s score is > the goal
+// 		Losses++
+// Call reset function
+
+// Create a click event
+// 	Grab the value of the crystal that was clicked on
+// 	Add that value to the player’s score
+// 	Call the win/loss function
